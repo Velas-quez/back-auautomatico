64dca86874b3c537c9eb84ecdfbdce14b30366fd
Velas <thiagovelasquez123@gmail.com>
2025-06-11 19:53:26 -0300

    add debug logs

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from flask import Flask, request, jsonify
 from apscheduler.schedulers.background import BackgroundScheduler
 import time
 import paho.mqtt.client as mqtt
+import logging
 
 app = Flask(__name__)
+app.logger.setLevel(logging.DEBUG)
 sched = BackgroundScheduler(daemon=True)
 sched.start()
+
+mqtt_client = mqtt.Client()
+mqtt_client.enable_logger()
 
 # MQTT HiveMQ Cloud
 BROKER = "40ee48e1dc244790b777f0d2e77cc32e.s1.eu.hivemq.cloud"
@@ -15,18 +20,30 @@ PASS = "?EW!oU*kj1ry7@0xSR8C"
 TOPIC = "casa/comando/led"
 
 def trigger_led():
-    client = mqtt.Client()
-    client.username_pw_set(USER, PASS)
-    client.tls_set()
-    client.connect(BROKER, PORT)
-    client.publish(TOPIC, "ON", qos=1)
-    client.disconnect()
+    app.logger.info("Função iniciada")
+    try:
+        client = mqtt.Client()
+        client.username_pw_set(USER, PASS)
+        client.tls_set()
+        client.connect(BROKER, PORT)
+        client.publish(TOPIC, "ON", qos=1)
+        client.disconnect()
+        app.logger.info("Função concluída com sucesso")
+    except Exception as e:
+        app.logger.error("Erro em trigger_led: %s", e, exc_info=True)
+
+
+@app.before_request
+def log_request_info():
+    app.logger.debug("Request headers: %s", request.headers)
+    app.logger.debug("Request body: %s", request.get_data())
 
 @app.route('/led-delay', methods=['POST'])
 def led_delay():
     run_time = time.time() + 5 * 60
     sched.add_job(trigger_led, 'date', run_date=time.strftime(
         '%Y-%m-%d %H:%M:%S', time.localtime(run_time)))
+    app.logger.info("Agendado acendimento para daqui 5 minutos")
     return jsonify({"scheduled_in": "5 minutes"})
 
 @app.route('/')
